a79e37ac85dcca4f3ce4ec774e821f69aea068c1
Robodiestrol <yaellg08@gmail.com>
2024-10-24 20:40:00 -0600

    ver-3

--- GIT/MainForm.cs
+++ GIT/MainForm.cs
@@ -42,7 +42,7 @@ namespace GIT
 			o=ap1.Substring(0,2);
 			i=ap2.Substring(0,1);
 			
-			label11.Text=o.ToUpper()+i+p+fech;
+			MessageBox.Show(nam.ToUpper()+" "+ap1.ToUpper()+" "+ap2.ToUpper()+" "+o.ToUpper()+i+p+fech);
 			
 			
 		}
